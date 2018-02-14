df0595c033b00c15ebfb28d2b95cda59d1535c69
coryswainston <swainston.cory89@gmail.com>
2018-02-13 20:09:26 -0700

    Basic home page

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Cory Swainston | Programmer</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:300,400|Rubik:300,400,900" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li>home</li>
+          <li>portfolio</li>
+          <li>contact</li>
+        </ul>
+      </nav>
+    </header>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php require 'header.php' ?>
+    <main>
+      <h1>Cory Swainston<span> | programmer</span></h1>
+      <h2>Just a student working hard to be more than a student.</h2>
+    </main>
+<?php require 'footer.php'; ?>
