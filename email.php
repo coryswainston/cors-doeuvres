550bf27d90d941218c12f7863c4e54fbd605cb63
coryswainston <swainston.cory89@gmail.com>
2018-02-16 00:28:59 -0700

    Add mail addon

--- email.php
+++ email.php
@@ -1,10 +1,27 @@
 <?php
-  $name = htmlspecialchars($_POST('name'));
-  $email = htmlspecialchars($_POST('email'));
-  $message = htmlspecialchars($_POST('message'));
-  $headers = "From: admin@coryswainston.com\r\nreply-to: $email";
 
-  mail("swainston.cory89@gmail.com", "Message from $name", $message, $headers);
+  require("sendgrid-php/lib/SendGrid.php");
 
-  header('Location: /index.php?sent=true#contact');
+
+  $name = htmlspecialchars($_POST['name']);
+  $email = htmlspecialchars($_POST['email']);
+  $message = htmlspecialchars($_POST['message']);
+
+
+  $from = new SendGrid\Email(null, "swainston.cory89@gmail.com");
+  $subject = "Message from $name at $email";
+  $to = new SendGrid\Email(null, "swainston.cory89@gmail.com");
+  $content = new SendGrid\Content("text/plain", $message);
+  $mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+  $apiKey = getenv('SENDGRID_API_KEY');
+  $sg = new \SendGrid($apiKey);
+
+  $response = $sg->client->mail()->send()->post($mail);
+  echo $response->statusCode();
+  echo $response->headers();
+  echo $response->body();
+
+  header('Location: index.php?sent=true#contact');
+  die();
  ?>
